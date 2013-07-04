58c0347a4fa411af60ee7a44c85042100b57201e
John Ky <newhoggy@gmail.com>
2013-07-04 21:24:56 +1000

    Finger tree data structures.

--- phalange/src/main/scala/myscalaz/a/FingerTree.scala
+++ phalange/src/main/scala/myscalaz/a/FingerTree.scala
@@ -1,54 +1,29 @@
 package myscalaz.a
 
-import scalaz.Semigroup
+trait Digit[+A]
+case class One[A](a: A) extends Digit[A]
+case class Two[A](a: A, b: A) extends Digit[A]
+case class Three[A](a: A, b: A, c: A) extends Digit[A]
+case class Four[A](a: A, b: A, c: A, d: A) extends Digit[A]
 
-sealed abstract class Finger[V, A] {
-  def foldMap[B](f: A => B)(implicit m: Semigroup[B]): B
+trait FingerTree[+A]
+case object Empty extends FingerTree[Nothing]
+case class Single[+A](a: A) extends FingerTree[A]
+case class Deep[A](l: Digit[A], m: FingerTree[Node[A]], r: Digit[A]) extends FingerTree[A]
 
-  /**
-   * Append the given element to the right
-   *
-   * @throws if the finger is `Four`.
-   */
-  def +:(a: => A): Finger[V, A]
+trait Node[+A]
+case class Node2[A](a: A, b: A) extends Node[A]
+case class Node3[A](a: A, b: A, c: A) extends Node[A]
 
-  /**
-   * Prepends the given element to the left
-   *
-   * @throws if the finger is `Four`.
-   */
-  def :+(a: => A): Finger[V, A]
-
-  /** Replaces the first element of this finger with `a` */
-  def |-:(a: => A): Finger[V, A]
-
-  /** Replaces the last element of this finger with `a` */
-  def :-|(a: => A): Finger[V, A]
-
-  def lhead: A
-
-  def ltail: Finger[V, A]
-
-  def rhead: A
-
-  def rtail: Finger[V, A]
-
-//  def toTree: FingerTree[V, A]
-
-//  def map[B, V2](f: A => B)(implicit m: Reducer[B, V2]): Finger[V2, B]
-
-  /** Apply the given side effect to each element. */
-  def foreach(f: A => Unit): Unit
-
-  /** An iterator that visits each element. */
-  def iterator: Iterator[A]
-
-  /** An iterator that visits each element in reverse order. */
-  def reverseIterator: Iterator[A]
-
-  def measure: V
-
-//  def toList: List[A] = map(x => x)(Reducer.ListReducer[A]).measure
-
-//  private[scalaz] def split1(pred: V => Boolean, accV: V): (Option[Finger[V, A]], A, Option[Finger[V, A]])
+object Data {
+  val x: FingerTree[Char] = Deep(Two('t', 'h'), Empty, Three('r', 'e', 'e'))
+  val y: FingerTree[Char] = {
+      Deep(
+          Two('t', 'h'),
+          Deep(
+              Two(Node2('i', 's'), Node2('i', 's')),
+              Empty,
+              Two(Node3('n', 'o', 't'), Node2('a', 't'))),
+          Three('r', 'e', 'e'))
+  }
 }
