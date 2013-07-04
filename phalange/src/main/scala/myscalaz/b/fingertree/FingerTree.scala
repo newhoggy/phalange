6a046baf08cfc8972140566c6f7b7a488179f35e
John Ky <newhoggy@gmail.com>
2013-07-05 00:05:04 +1000

    Add missing case for :+ and +:.

--- phalange/src/main/scala/myscalaz/b/fingertree/FingerTree.scala
+++ phalange/src/main/scala/myscalaz/b/fingertree/FingerTree.scala
@@ -22,15 +22,17 @@ case class D4[A](a: A, b: A, c: A, d: A ) extends Digit[A]
 
 trait FingerTree[+A] {
   def +:[B >: A](x: B): FingerTree[B] = this match {
-    case Empty => Single(x)
-    case Single(v) => Deep(D1(x), Empty, D1(v))
-    case Deep(l, m, r) => Deep(x +: l, m, r)
+    case Empty                      => Single(x)
+    case Single(v)                  => Deep(D1(x)   , Empty           , D1(v) )
+    case Deep(D4(a, b, c, d), m, r) => Deep(D2(a, b), N3(c, d, x) +: m, r     )
+    case Deep(l             , m, r) => Deep(x +: l  , m               , r     )
   }
 
   def :+[B >: A](x: B): FingerTree[B] = this match {
-    case Empty => Single(x)
-    case Single(v) => Deep(D1(v), Empty, D1(x))
-    case Deep(l, m, r) => Deep(l, m, r :+ x)
+    case Empty                      => Single(x)
+    case Single(v)                  => Deep(D1(v) , Empty           , D1(x)   )
+    case Deep(l, m, D4(a, b, c, d)) => Deep(l     , m :+ N3(a, b, c), D2(d, x))
+    case Deep(l, m, r             ) => Deep(l     , m               , r :+ x  )
   }
 }
 
