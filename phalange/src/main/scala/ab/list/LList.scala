fb027048531df48eb85a655bd0b0fd8cf091fd9f
John Ky <newhoggy@gmail.com>
2013-07-05 20:33:46 +1000

    Define foldLeft method.

--- phalange/src/main/scala/ab/list/LList.scala
+++ phalange/src/main/scala/ab/list/LList.scala
@@ -1,9 +1,16 @@
 package ab.list
 
+import scala.annotation.tailrec
+
 trait LList[+A] {
   def head: A
   def tail: LList[A]
   def +:[B >: A](item: B): LList[B] = Cons(item, this)
+  @tailrec
+  final def foldLeft[B](z: B)(f: (B, A) => B): B = this match {
+    case Cons(head, tail) => tail.foldLeft(f(z, head))(f)
+    case Nil => z
+  }
 }
 
 case object Nil extends LList[Nothing] {
@@ -17,5 +24,6 @@ object LListData {
   def main(args: Array[String]): Unit = {
     val empty: LList[Int] = Nil
     val single: LList[Int] = 1 +: Nil
+    List().foldLeft(???)(???)
   }
 }
