d3e8d0ffd259899c6bd4edee24f636a84f4a3f50
John Ky <newhoggy@gmail.com>
2013-07-08 01:05:22 +1000

    Add comment.

--- phalange/src/main/scala/cc/fingertree/ReduceOps.scala
+++ phalange/src/main/scala/cc/fingertree/ReduceOps.scala
@@ -5,5 +5,6 @@ import scalaz.syntax.Ops
 trait ReduceOps[F[_], A] extends Ops[F[A]] {
   implicit def F: Reduce[F]
   
+  // Note: Renamed from toList to asList to avoid conflict with built-in Scala toList method
   def asList: List[A] = F.reduceR[A, List[A]](_::_)(Nil)(self)
 }
