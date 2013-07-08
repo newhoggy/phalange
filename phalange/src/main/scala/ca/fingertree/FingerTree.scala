c5f2d17828ab58f5271dcff12dfb073aa52285a8
John Ky <newhoggy@gmail.com>
2013-07-08 10:10:36 +1000

    Remove View from cc.

--- phalange/src/main/scala/ca/fingertree/FingerTree.scala
+++ phalange/src/main/scala/ca/fingertree/FingerTree.scala
@@ -1,6 +1,9 @@
 package ca.fingertree
 
 trait FingerTree[+A]
+
 case object Empty extends FingerTree[Nothing]
+
 case class Single[+A](a: A) extends FingerTree[A]
+
 case class Deep[A](l: Digit[A], m: FingerTree[Node[A]], r: Digit[A]) extends FingerTree[A]
