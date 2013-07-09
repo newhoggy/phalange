17d9167b7a4f9ca98b06fa4d060696c3494ef28b
John Ky <newhoggy@gmail.com>
2013-07-09 10:36:01 +1000

    Helper methods for ViewL.

--- phalange/src/main/scala/cf/fingertree/ViewL.scala
+++ phalange/src/main/scala/cf/fingertree/ViewL.scala
@@ -1,13 +1,21 @@
 package cf.fingertree
 
-trait ViewL[S[_], +A] {
-//  def fold[B](b: => B, f: (=> A, => S[A]) => B): B
-//  def headOption: Option[A] = fold(None, (a, sa) => Some(a))
-//  def tailOption: Option[S[A]] = fold(None, (a, sa) => Some(sa))
-//  def head: A = headOption.getOrElse(sys.error("Head on empty view"))
-//  def tail: S[A] = tailOption.getOrElse(sys.error("Tail on empty view"))
+trait ViewL[S[+_], +A] {
+  def headOption: Option[A] = this match {
+    case EmptyL() => None
+    case ConsL(a, sa) => Some(a)
+  }
+
+  def tailOption: Option[S[A]] = this match {
+    case EmptyL() => None
+    case ConsL(a, sa) => Some(sa)
+  }
+
+  def head: A = headOption.getOrElse(sys.error("Head on empty view"))
+
+  def tail: S[A] = tailOption.getOrElse(sys.error("Tail on empty view"))
 }
 
-case class EmptyL[S[_], A]() extends ViewL[S, A]
+case class EmptyL[S[+_], +A]() extends ViewL[S, A]
 
-case class ConsL[S[_], A](a: A, sa: S[A]) extends ViewL[S, A]
+case class ConsL[S[+_], +A](a: A, sa: S[A]) extends ViewL[S, A]
