f57e9c0fba25ee26a42724fc1b133e2d6aa30b93
John Ky <newhoggy@gmail.com>
2013-07-11 20:22:48 +1000

    Fix warnings.

--- cc/src/main/scala/fingertree/Reduce.scala
+++ cc/src/main/scala/fingertree/Reduce.scala
@@ -1,5 +1,7 @@
 package fingertree
 
+import scala.language.higherKinds
+
 trait Reduce[F[_]]  { self =>
   def reduceR[A, B](f: (A, => B) => B)(fa: F[A])( z: => B): B
   def reduceL[A, B](f: (B,    A) => B)(z:    B )(fa: F[A]): B
