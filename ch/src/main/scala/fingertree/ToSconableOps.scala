15746071549161112aa3c9747e786a5635951695
John Ky <newhoggy@gmail.com>
2013-07-11 18:39:36 +1000

    Fix warnings.

--- ch/src/main/scala/fingertree/ToSconableOps.scala
+++ ch/src/main/scala/fingertree/ToSconableOps.scala
@@ -1,5 +1,7 @@
 package fingertree
 
+import scala.language.implicitConversions
+
 trait ToSconableOps {
   implicit def ToSconableOps[S, A](v: S)(implicit F0: Sconable[S, A]) = new SconableOps[S, A] {
     def self = v
