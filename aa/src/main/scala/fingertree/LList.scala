df8e7f26bd91f433a9bb002eac24403634e306f3
John Ky <newhoggy@gmail.com>
2013-07-16 23:16:55 +1000

    Implement foldLeft.

--- aa/src/main/scala/fingertree/LList.scala
+++ aa/src/main/scala/fingertree/LList.scala
@@ -1,9 +1,16 @@
 package fingertree
 
+import scala.annotation.tailrec
+
 trait List[+A] {
   def head: A
   def tail: List[A]
   def +:[B >: A](item: B): List[B] = Cons(item, this)
+  @tailrec
+  final def foldLeft[B](f: (A, => B) => B)(b: B): B = this match {
+    case Nil => b
+    case Cons(head, tail) => tail.foldLeft(f)(f(head, b))
+  }
 }
 
 case object Nil extends List[Nothing] {
