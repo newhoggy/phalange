543ec469a8d3631f509f232b4dfd376ed83923f8
John Ky <newhoggy@gmail.com>
2013-07-18 00:13:26 +1000

    Use nicer syntax.

--- ce/src/main/scala/fingertree/Implicits.scala
+++ ce/src/main/scala/fingertree/Implicits.scala
@@ -31,17 +31,25 @@ object Implicits {
   }
 
   implicit object ReduceDigit extends Reduce[Digit] {
-    override def reduceR[A, B](f: (A, => B) => B)(fa: Digit[A])(z: => B): B = fa match {
-      case D1(a         ) =>           f(a, z)
-      case D2(a, b      ) =>      f(a, f(b, z))
-      case D3(a, b, c   ) => f(a, f(b, f(c, z)))
-      case D4(a, b, c, d) => !!!
+    import Syntax._
+    override def reduceR[A, B](f: (A, => B) => B)(fa: Digit[A])(z: => B): B = {
+      implicit val BConsable = Consable(f)
+      fa match {
+        case D1(a         ) =>           a +: z
+        case D2(a, b      ) =>      a +: b +: z
+        case D3(a, b, c   ) => a +: b +: c +: z
+        case D4(a, b, c, d) => !!!
+      }
     }
-    override def reduceL[A, B](f: (B,    A) => B)(z:    B)(fa: Digit[A]): B = fa match {
-      case D1(a         ) =>     f(z, a)
-      case D2(a, b      ) =>   f(f(z, a), b)
-      case D3(a, b, c   ) => f(f(f(z, a), b), c)
-      case D4(a, b, c, d) => !!!
+    
+    override def reduceL[A, B](f: (B,    A) => B)(z:    B)(fa: Digit[A]): B = {
+      implicit val BConsable = Sconable(f)
+      fa match {
+        case D1(a         ) => z :+ a
+        case D2(a, b      ) => z :+ a :+ b
+        case D3(a, b, c   ) => z :+ a :+ b :+ c
+        case D4(a, b, c, d) => !!!
+      }
     }
   }
 
