e1ded9c6f294223b54ec64d078520b45e4f9d4cb
John Ky <newhoggy@gmail.com>
2013-07-18 00:37:01 +1000

    Synchronize

--- cd/src/main/scala/fingertree/Reduce.scala
+++ cd/src/main/scala/fingertree/Reduce.scala
@@ -4,5 +4,5 @@ import scala.language.higherKinds
 
 trait Reduce[F[_]]  { self =>
   def reduceR[A, B](f: (A, => B) => B)(fa: F[A])( z: => B): B
-  def reduceL[A, B](f: (B,    A) => B)( z:    B)(fa: F[A]): B
+  def reduceL[A, B](f: (B,    A) => B)( z:   B )(fa: F[A]): B
 }
