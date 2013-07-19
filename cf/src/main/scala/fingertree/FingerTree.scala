714ab541ad73cc51a22ea6170c2972e160c411e1
John Ky <newhoggy@gmail.com>
2013-07-19 20:10:52 +1000

    Tidy up.

--- cf/src/main/scala/fingertree/FingerTree.scala
+++ cf/src/main/scala/fingertree/FingerTree.scala
@@ -3,16 +3,16 @@ package fingertree
 trait FingerTree[+A] {
   def +:[B >: A](x: B): FingerTree[B] = this match {
     case Empty                      => Single(x)
-    case Single(v)                  => Deep(D1(x)   , Empty           , D1(v) )
-    case Deep(D4(a, b, c, d), m, r) => Deep(D2(x, a), N3(b, c, d) +: m, r     )
-    case Deep(l             , m, r) => Deep(x +: l  , m               , r     )
+    case Single(v)                  => Deep(D1(x   ), Empty           , D1(v))
+    case Deep(D4(a, b, c, d), m, r) => Deep(D2(x, a), N3(b, c, d) +: m, r    )
+    case Deep(l             , m, r) => Deep(x +: l  , m               , r    )
   }
 
   def :+[B >: A](x: B): FingerTree[B] = this match {
     case Empty                      => Single(x)
-    case Single(v)                  => Deep(D1(v) , Empty           , D1(x)   )
-    case Deep(l, m, D4(a, b, c, d)) => Deep(l     , m :+ N3(a, b, c), D2(d, x))
-    case Deep(l, m, r             ) => Deep(l     , m               , r :+ x  )
+    case Single(v)                  => Deep(D1(v), Empty           , D1(x   ))
+    case Deep(l, m, D4(a, b, c, d)) => Deep(l    , m :+ N3(a, b, c), D2(d, x))
+    case Deep(l, m, r             ) => Deep(l    , m               , r :+ x  )
   }
   
   def viewL: ViewL[FingerTree, A] = this match {
