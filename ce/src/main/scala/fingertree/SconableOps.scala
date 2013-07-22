b035ff1d4dd4dd7c1945b81d7412758ed0c74c52
John Ky <newhoggy@gmail.com>
2013-07-22 22:11:13 +1000

    Tidy up.

--- ce/src/main/scala/fingertree/SconableOps.scala
+++ ce/src/main/scala/fingertree/SconableOps.scala
@@ -5,5 +5,5 @@ import scalaz.syntax.Ops
 trait SnocableOps[S, A] extends Ops[S] {
   implicit def F: Snocable[S, A]
 
-  def :+(a: A) : S = F.snoc(self, a)
+  def :+(a: A): S = F.snoc(self, a)
 }
--- cf/src/main/scala/fingertree/ConsableOps.scala
+++ cf/src/main/scala/fingertree/ConsableOps.scala
@@ -5,5 +5,5 @@ import scalaz.syntax.Ops
 trait ConsableOps[A, S] extends Ops[S] {
   implicit def F: Consable[A, S]
 
-  def +:(a: A) : S = F.cons(a, self)
+  def +:(a: A): S = F.cons(a, self)
 }
