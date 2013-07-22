27854f5f857a664ae0d31c62e9d1e7b6693cdf4e
John Ky <newhoggy@gmail.com>
2013-07-22 10:25:55 +1000

    Tidy up.

--- cb/src/main/scala/fingertree/Node.scala
+++ cb/src/main/scala/fingertree/Node.scala
@@ -2,5 +2,5 @@ package fingertree
 
 trait Node[+A]
 
-case class N2[A](a: A, b: A       ) extends Node[A]
-case class N3[A](a: A, b: A, c: A ) extends Node[A]
+case class N2[A](a: A, b: A      ) extends Node[A]
+case class N3[A](a: A, b: A, c: A) extends Node[A]
